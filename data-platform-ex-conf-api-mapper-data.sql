eca81a02e4e6a07b0eec80e22f9c094b30af2632
MiyuYokoyama0109 <miyu.y@latona.io>
2022-11-10 18:39:14 +0900

    release

--- data-platform-ex-conf-api-mapper-data.sql
+++ data-platform-ex-conf-api-mapper-data.sql
@@ -1,13 +1,14 @@
 CREATE TABLE `data_platform_ex_conf_api_mapper_data`
 (
-    `APIServiceName`                 varchar(100) NOT NULL,  
-    `APIType`                        varchar(10) NOT NULL,  
-    `APIName`                        varchar(100) NOT NULL,  
-    `Field`                          varchar(100) NOT NULL,  
-    `ExConfAPIServiceName`           varchar(100) DEFAULT NULL,  
-    `Tabletag`                       varchar(100) DEFAULT NULL, 
-    `TableConfirmed`                 varchar(100) DEFAULT NULL,  
-    `ExConfAPIType`                  varchar(10) DEFAULT NULL, 
-    PRIMARY KEY (`APIServiceName`, `APIType`, `APIName`,`Field`)
+    `ServiceLabel`                   varchar(50) NOT NULL,
+    `APIType`                        varchar(10) NOT NULL,
+    `APIName`                        varchar(200) NOT NULL,
+    `Field`                          varchar(100) NOT NULL,
+    `ExConfAPIServiceName`           varchar(100) DEFAULT NULL,
+    `ExConfAPIName`                  varchar(200) DEFAULT NULL,  
+    `Tabletag`                       varchar(100) DEFAULT NULL,
+    `TableConfirmed`                 varchar(100) DEFAULT NULL,
+    `ExConfAPIType`                  varchar(10) DEFAULT NULL,
+    PRIMARY KEY (`ServiceLabel`, `APIType`, `APIName`,`Field`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
